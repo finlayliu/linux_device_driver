59e43a8a4033fc01704a581f325701afb08ae1cf
finlay <liufy.thu@gmail.com>
2015-02-05 00:14:03 +0800

    added char module

--- ldd/char.h
+++ ldd/char.h
@@ -4,5 +4,18 @@
 #define SCULLC_MAJOR 0   /* dynamic major by default */
 
 #define SCULLC_DEVS 4    /* scullc0 through scullc3 */
+#define SCULLC_QUANTUM  4000 /* use a quantum size like scull */
+#define SCULLC_QSET     500
+
+struct scullc_dev {
+	void **data;
+	struct scullc_dev *next;  /* next listitem */
+	int vmas;                 /* active mappings */
+	int quantum;              /* the current allocation size */
+	int qset;                 /* the current array size */
+	size_t size;              /* 32-bit will suffice */
+	struct semaphore sem;     /* Mutual exclusion */
+	struct cdev cdev;
+};
 
 #endif
--- ldd/char_main.c
+++ ldd/char_main.c
@@ -1,9 +1,50 @@
-#include <module.h>
-#include <init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
 #include <linux/types.h>	/* size_t */
+#include <linux/slab.h>		/* kmalloc() */
+#include <linux/errno.h>	/* error codes */
+#include <linux/fs.h>		/* everything... */
+#include <linux/kernel.h>	/* printk() */
+
+
+
+
 
 int scullc_major = SCULLC_MAJOR;
 int scullc_devs  = SCULLC_DEVS;		/* number of bare scullc devices */
+int scullc_quantum = SCULLC_QUANTUM;
+int scullc_qset =    SCULLC_QSET;
+
+
+
+struct scullc_dev *scullc_devices; /* allocated in scullc_init */
+
+struct file_operations scullc_fops = {
+	.owner =     THIS_MODULE,
+	.llseek =    scullc_llseek,
+	.read =	     scullc_read,
+	.write =     scullc_write,
+	.ioctl =     scullc_ioctl,
+	.open =	     scullc_open,
+	.release =   scullc_release,
+	.aio_read =  scullc_aio_read,
+	.aio_write = scullc_aio_write,
+};
+
+
+static void scullc_setup_cdev(struct scullc_dev *dev, int index)
+{
+	int err, devno = MKDEV(scullc_major, index);
+    
+	cdev_init(&dev->cdev, &scullc_fops);
+	dev->cdev.owner = THIS_MODULE;
+	dev->cdev.ops = &scullc_fops;
+	err = cdev_add (&dev->cdev, devno, 1);
+	/* Fail gracefully if need be */
+	if (err)
+		printk(KERN_NOTICE "Error %d adding scull%d", err, index);
+}
 
 
 int scullc_init(void)
@@ -20,7 +61,21 @@ int scullc_init(void)
 	if (result)
 		return result;
 
-	scullc_devices = 
+	scullc_devices = kmalloc(scullc_devs*sizeof(struct scullc_dev), GFP_KERNEL);
+	if (!scullc_devices) {
+		result = -ENOMEM;
+		goto fail_malloc;
+	}
+	
+	memset(scullc_devices, 0, scullc_devs*sizeof(struct scullc_dev));
+	for (i = 0; i < scullc_devs; i++) {
+		scullc_devices[i].quantum = scullc_quantum;
+		scullc_devices[i].qset = scullc_qset;
+		sema_init(&scullc_devices[i].sem, 1);
+		scullc_setup_cdev(scullc_devices + i, i);		
+	}
+		
+		
 	return result;
 }
 
